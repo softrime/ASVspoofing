37783da2fdb1796eb42d9cbb0e753252aa63f168
softrime <wsz353086175@icloud.com>
2018-01-05 17:24:24 +0800

    mfcc, cqcc

---
+++ Iter_cqcc.py
@@ -0,0 +1,382 @@
+import random
+import codecs
+import mxnet as mx
+import numpy as np
+import data_loader
+
+class SimpleBatch(object):
+    def __init__(self, data_names, data, label_names, label):
+        self.data = data
+        self.label = label
+        self.data_names = data_names
+        self.label_names = label_names
+
+        self.pad = 0
+        self.index = None
+
+    def provide_data(self):
+        return [(n, x.shape) for n, x in zip(self.data_names, self.data)]
+
+    def provide_label(self):
+        return [mx.io.DataDsec('softmax_label', self.label.shape)]
+
+
+class Test_Iter(mx.io.DataIter):
+    def __init__(self, feats_file, data_names, data_shapes, label_names, label_shapes, batch_size):
+        self._provide_data = zip(data_names, data_shapes)
+        self._provide_label = zip(label_names, label_shapes)
+        self.data_shapes = data_shapes
+        self.label_shapes = label_shapes
+        self.cur_batch = 0
+        self.feats_file = feats_file
+        self.batch_size = batch_size
+        self.features = data_loader.load_cqcc(test_file=self.feats_file, mode='D', do_shuffle=False)
+	self.num_batches = len(self.features) / self.batch_size
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def reset(self):
+        self.cur_batch = 0
+
+    @property
+    def provide_data(self):
+        return self._provide_data
+
+    @property
+    def provide_label(self):
+        return self._provide_label
+
+    def next(self):
+        if self.cur_batch == self.num_batches:
+	    raise StopIteration
+        data = [mx.nd.array(self.features[self.cur_batch * self.batch_size:(self.cur_batch + 1) * self.batch_size])]
+        label = [mx.nd.full((self.batch_size, 2), 3)]
+        self.cur_batch += 1
+        return mx.io.DataBatch(data, label, pad=0)
+
+
+class Simple_Iter(mx.io.DataIter):
+    def __init__(self, feats_scp, labels_scp, data_names, data_shapes, label_names, label_shapes, batch_size):
+        self._provide_data  = zip(data_names, data_shapes)
+        self._provide_label = zip(label_names, label_shapes)
+        self.data_shapes = data_shapes
+        self.label_shapes = label_shapes
+        self.cur_batch = 0
+        self.feats_scp = feats_scp
+        self.labels_scp = labels_scp
+        self.batch_size = batch_size
+	#self.features, self.labels = data_loader.loader_by_frames(feats_scp=self.feats_scp, labels_scp=self.labels_scp)
+        self.features, self.labels = data_loader.load_cqcc(feat_scp=self.feats_scp, label_scp=self.labels_scp)
+	self.scale = 50
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        return self.next()
+
+    def reset(self):
+        self.cur_batch = 0
+
+    @property
+    def provide_data(self):
+        return self._provide_data
+
+    @property
+    def provide_label(self):
+        return self._provide_label
+
+    def next(self):
+    	if len(self.features)/self.scale>((self.cur_batch + 1) * self.batch_size):
+            data = [mx.nd.array(self.features[self.cur_batch * self.batch_size:(self.cur_batch + 1) * self.batch_size])]
+            label = [mx.nd.array(self.labels[self.cur_batch * self.batch_size:(self.cur_batch + 1) * self.batch_size])]
+	    self.cur_batch += 1
+            return mx.io.DataBatch(data, label)
+
+        elif len(self.features)/self.scale<(self.cur_batch * self.batch_size):
+            raise StopIteration
+
+	elif len(self.features)/self.scale<=((self.cur_batch + 1) * self.batch_size):
+            residual = ((self.cur_batch + 1) * self.batch_size) - len(self.features)
+            pad = 0
+            data = [mx.nd.array(self.features[self.cur_batch * self.batch_size:] + [np.full((self.data_shapes[0][1],), pad)] * residual)]
+            label = [mx.nd.array(self.labels[self.cur_batch * self.batch_size:] + [np.full((self.label_shapes[0][1],), pad)] * residual)]
+	    self.cur_batch += 1
+            return mx.io.DataBatch(data, label)
+
+        else:
+            print "something wrong happened in generate batches"
+            exit(1)
+
+
+
+class multi_input_Iter(mx.io.DataIter):
+
+    def __init__(self, feats_scp, labels_scp, batch_size, feat_dim, label_dim, num_hidden, shuffle=True, mode="train"):
+
+        self.mode           = mode
+        self.num_hidden     = num_hidden
+        self.data_name      = "data"
+        self.label_name     = "softmax_label"
+        self.sent_name      = "sentidx"
+        self.feats_scp      = feats_scp
+        self.labels_scp     = labels_scp
+
+        self.feat_dim       = feat_dim
+        self.label_dim      = label_dim
+
+
+        self.batch_size     = batch_size
+        self.shuffle        = shuffle
+
+        self.data           = [mx.nd.zeros((self.batch_size, self.feat_dim-1))]
+        self.data.append(mx.nd.zeros((self.batch_size, self.num_hidden)))
+        for i in range(10):
+            self.data.append(mx.nd.full((self.batch_size, self.num_hidden), i+1.0))
+        self.label          = [mx.nd.zeros((self.batch_size, self.label_dim))]
+
+        self.sents          = []
+        for i in range(10):
+            self.sents.append("sent%d" % (i+1))
+        self.data_names     = [self.data_name, self.sent_name] + self.sents
+        self.label_names    = [self.label_name]
+
+        self._provide_data  = [mx.io.DataDesc(self.data_name, self.data[0].shape),
+                               mx.io.DataDesc(self.sent_name, self.data[1].shape)]
+        for i in range(10):
+            self._provide_data.append(mx.io.DataDesc(self.sents[i], self.data[i+2].shape))
+
+        self._provide_label = [(self.label_name, self.label[0].shape)]
+
+        self.pack()
+        self.reset()
+
+
+    def __iter__(self):
+
+        return self
+
+
+    def pack(self):
+        ''' packing data to batches '''
+
+        fin = codecs.open(self.feats_scp);featfiles   = fin.readlines();fin.close()
+        fin = codecs.open(self.labels_scp);labelfiles = fin.readlines();fin.close()
+        assert(len(featfiles)==len(labelfiles))
+
+        self.batches = []
+        self.sent    = []
+        self.pads    = []
+        features     = []
+        labels       = []
+        sents     = []
+        alldata         = []
+
+        for f in range(len(featfiles)):
+
+
+            temp_features = []
+            temp_labels   = []
+            temp_sents    = []
+            temp_data     = []
+            temp_batch    = []
+
+
+            path = file(featfiles[f].strip().split('\n')[0], "rb")
+            feat = np.load(path)
+            path.close()
+            basename_f = featfiles[f].strip().split('/')[-1].split('\n')[0].split('.')[0]
+            print "process %s "% basename_f
+
+            path  = file(labelfiles[f].strip().split('\n')[0], "rb")
+            label = np.load(path)
+            path.close()
+            basename_l = labelfiles[f].strip().split('/')[-1].split('\n')[0].split('.')[0]
+            print "process %s "% basename_l
+
+            assert(basename_f == basename_l)
+
+            nframe = feat.shape[0]
+            for j in range(0, nframe):
+
+                if self.mode == "train":
+
+                    features.append(feat[j][:-1])
+                    labels.append(label)
+                    sents.append(feat[j][-1])
+                    alldata.append((feat[j][:-1], label, feat[j][-1]))
+
+                elif self.mode == "sent":
+
+                    temp_features.append(feat[j][:-1])
+                    temp_labels.append(label)
+                    temp_sents.append(feat[j][-1])
+                    temp_data.append((feat[j][:-1], label, feat[j][-1]))
+
+                else:
+
+                    print "Error: unknown mode!"
+                    exit(1)
+
+            if self.mode == "sent":
+
+                temp_data  = np.array(temp_data)
+
+                nbatch = len(temp_data) / self.batch_size
+                residual = len(temp_data) - nbatch * self.batch_size
+
+                for i in range(nbatch):
+                    np_data_buffer = np.zeros((self.batch_size, self.feat_dim-1))
+                    np_label_buffer = np.zeros((self.batch_size, self.label_dim))
+                    np_sent_buffer = np.zeros((self.batch_size, self.num_hidden))
+                    for x in range(self.batch_size):
+                        np_data_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 0][x]
+                        np_label_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 1][x]
+                        np_sent_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 2][x]
+                    self.data[0][:] = np_data_buffer
+                    self.data[1][:] = np_sent_buffer
+                    self.label[0][:] = np_label_buffer
+                    tdata  = []
+                    tlabel = []
+
+                    for k in range(len(self.data)):
+                        tdata.append(self.data[k].copy())
+                    for k in range(len(self.label)):
+                        tlabel.append(self.label[k].copy())
+                    temp_batch.append((tdata, tlabel))
+
+                if residual > 0:
+                    residual_feats  = np.concatenate((temp_features[-residual:], np.zeros((self.batch_size - residual, self.feat_dim-1))))
+                    residual_labels = np.concatenate((temp_labels[-residual:], np.zeros((self.batch_size - residual, self.label_dim))))
+                    residual_sents  = np.concatenate((temp_sents[-residual:], np.ones((self.batch_size - residual))))
+                    assert(residual_sents.shape[0]==residual_labels.shape[0])
+                    residual_batch  = []
+
+                    assert(residual_feats.shape[0]==residual_labels.shape[0])
+                    np_data_buffer = np.zeros((self.batch_size, self.feat_dim-1))
+                    np_label_buffer = np.zeros((self.batch_size, self.label_dim))
+                    np_sent_buffer = np.zeros((self.batch_size, self.num_hidden))
+                    for x in range(self.batch_size):
+                        np_data_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 0][x]
+                        np_label_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 1][x]
+                        np_sent_buffer[x][:] = temp_data[i*self.batch_size:(i+1)*self.batch_size, 2][x]
+
+
+                    self.data[0][:] = np_data_buffer
+                    self.data[1][:] = np_sent_buffer
+                    self.label[0][:] = np_label_buffer
+
+                    tdata  = []
+                    tlabel = []
+
+                    for k in range(len(self.data)):
+                        tdata.append(self.data[k].copy())
+                    for k in range(len(self.label)):
+                        tlabel.append(self.label[k].copy())
+                    temp_batch.append((tdata, tlabel))
+
+
+                self.sent.append(temp_batch)
+                self.pads.append(residual)
+
+
+
+
+        if self.mode == "train":
+
+            if self.shuffle == True:
+
+                random.shuffle(alldata)
+
+            alldata    = np.array(alldata)
+            batches     = []
+            nbatch = len(alldata)/self.batch_size
+
+            np_data_buffer = np.zeros((self.batch_size, self.feat_dim-1))
+            np_label_buffer = np.zeros((self.batch_size, self.label_dim))
+            np_sent_buffer = np.zeros((self.batch_size, self.num_hidden))
+
+            for i in range(nbatch):
+                for x in range(self.batch_size):
+                    np_data_buffer[x][:] = alldata[i*self.batch_size:(i+1)*self.batch_size, 0][x]
+                    np_label_buffer[x][:] = alldata[i*self.batch_size:(i+1)*self.batch_size, 1][x]
+                    np_sent_buffer[x][:] = alldata[i*self.batch_size:(i+1)*self.batch_size, 2][x]
+                self.data[0][:] = np_data_buffer
+                self.data[1][:] = np_sent_buffer
+                self.label[0][:] = np_label_buffer
+                tdata  = []
+                tlabel = []
+
+
+                for k in range(len(self.data)):
+                    tdata.append(self.data[k].copy())
+                for k in range(len(self.label)):
+                    tlabel.append(self.label[k].copy())
+                self.batches.append((tdata, tlabel))
+
+
+        self.num_batches = len(self.batches)
+        print "finish packing"
+
+
+
+    def reset(self):
+
+        self.cur_batch = -1
+        self.cur_sent  = -1
+
+
+    def __next__(self):
+
+        return self.next()
+
+
+    @property
+    def provide_data(self):
+
+        return self._provide_data
+
+
+    @property
+    def provide_label(self):
+
+        return self._provide_label
+
+
+    def next(self):
+
+        if self.mode == "sent" and self.cur_sent < (len(self.sent)-1):
+
+            self.nsent = len(self.sent)
+
+            batches_per_sent = []
+        for b in range(len(self.sent[self.cur_sent])):
+            mdata = self.sent[self.cur_sent][b][0]
+            mlabel = self.sent[self.cur_sent][b][1]
+
+            data_batch = SimpleBatch(self.data_names, mdata, self.label_names, mlabel)
+            batches_per_sent.append(data_batch)
+
+            self.cur_sent += 1
+
+            return (batches_per_sent, self.pads[self.cur_sent])
+
+
+        if self.mode == "train" and self.cur_batch < len(self.batches)-1:
+
+            self.cur_batch += 1
+            mdata = self.batches[self.cur_batch][0]
+            mlabel = self.batches[self.cur_batch][1]
+
+            data_batch = SimpleBatch(self.data_names, mdata, self.label_names, mlabel)
+
+            return data_batch
+
+        else:
+
+            raise StopIteration
+
---
+++ train_cqcc.py
@@ -0,0 +1,291 @@
+
+import os, sys
+import time
+import codecs
+import random
+import logging
+
+import numpy as np
+import mxnet as mx
+from mxnet import nd
+
+sys.path.append('.')
+from Iter_cqcc import Simple_Iter
+from Iter_cqcc import Test_Iter
+import model
+##### metric define #####
+def MSE(labels, preds):
+        labels = labels.reshape((-1,))
+        preds = preds.reshape((-1,))
+        loss = 0.
+        num_inst = 0
+        loss = ((labels - preds) ** 2).sum(0)
+        #if loss < 1:
+        #    print labels, preds, loss;raw_input('why so small mse')
+        num_inst = labels.shape[0]
+        return loss
+
+
+##### scp file #####
+data_names = ["mfcc-static_dim-13", "mfcc-static_dim-14-speaker_index"]
+data_name = data_names[0]
+train_feats_scp  = "./scp/%s/feats/train.scp" % data_name 
+dev_feats_scp    = "./scp/%s/feats/dev.scp" % data_name
+eval_feats_scp   = "./scp/%s/feats/eval.scp" % data_name
+
+train_labels_scp = "./scp/%s/labels/train.scp" % data_name
+dev_labels_scp   = "./scp/%s/labels/dev.scp" % data_name
+
+
+
+##### parameters config #####
+#data_dim   = 13
+data_dim   = 90
+label_dim  = 2
+batch_size = 4
+lr         = 0.0001
+num_epoch  = 1
+ndnn       = 4
+dnnsize    = 2048
+pfix       = "spoofing_cqcc_arc-%dx%d_lr-%f_batchsize-%d"% (ndnn, dnnsize, lr, batch_size)
+prefix     = "checkpoints/spoofing_cqcc_arc-%dx%d_lr-%f_batchsize-%d" % (ndnn, dnnsize, lr, batch_size)
+
+
+##### log deploy #####
+logfn      = "LOG/spoofing_mfcc_arc-%dx%d_lr-%f_batchsize-%d_epoch-%d_datad2.log" % (ndnn, dnnsize, lr, batch_size, num_epoch)
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)-15s %(message)s',
+                    datefmt='%a, %d %b %Y %H:%M:%S',
+                    filename=logfn,
+                    filemode='w')
+console    = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(message)s')
+
+
+##### create dataIter #####
+data_names = ['data']
+label_names = ['softmax_label']
+data_shapes = [(batch_size, data_dim)]
+label_shapes = [(batch_size, label_dim)]
+#train_Iter = Iter(train_feats_scp, train_labels_scp, batch_size, data_dim, label_dim, dnnsize, True, mode="train")
+#dev_Iter   = Iter(dev_feats_scp, dev_labels_scp, batch_size, data_dim, label_dim, dnnsize, False, mode="train")
+train_Iter = Simple_Iter(train_feats_scp, train_labels_scp, data_names, data_shapes, label_names, label_shapes, batch_size)
+dev_Iter = Simple_Iter(dev_feats_scp, dev_labels_scp, data_names, data_shapes, label_names, label_shapes, batch_size)
+print "finish iter"
+
+
+
+##### define NN #####
+net = model.dnn(ndnn=ndnn, dnnsize=dnnsize)
+
+##### create and train #####
+data_names = [x[0] for x in train_Iter.provide_data]
+#print data_names;raw_input()
+label_names = [x[0] for x in train_Iter.provide_label]
+mod = mx.mod.Module(symbol=net, context=mx.context.gpu(5), data_names=data_names,label_names=label_names)
+model_prefix = prefix
+checkpoint = mx.callback.do_checkpoint(model_prefix)
+print "finish model"
+
+
+def dev_loss(module, eval_data, eval_metric):
+    eval_data.reset()
+    eval_metric.reset()
+    for batch in eval_data:
+        module.forward(batch, is_train=False)
+	module.update_metric(eval_metric, batch.label)
+	outputs = module.get_outputs()
+
+'''
+mod.bind(data_shapes=train_Iter.provide_data, label_shapes=train_Iter.provide_label)
+mod.init_params(initializer=mx.init.Uniform(scale=.1))
+metric = mx.metric.create(MSE)
+mod.init_optimizer(optimizer='sgd', optimizer_params=(('learning_rate', lr), ))
+
+metric.reset()
+dev_loss(mod, train_Iter, metric)
+for name, val in metric.get_name_value():
+    logging.info('Initialization Train-%s=%f', name, val)
+metric.reset()
+dev_loss(mod, dev_Iter, metric)
+for name, val in metric.get_name_value():
+    logging.info('Initialization Dev-%s=%f', name, val)
+epoch = 0
+last_acc = float("Inf")
+last_params = None
+while True:
+    print "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh new epoch hhhhhhhhhhhhhhhhhhhhhhhhhhhhh"
+    tic = time.time()
+    train_Iter.reset()
+    metric.reset()
+    for batch in train_Iter:
+    	mod.forward(batch, is_train=True)
+	mod.update_metric(metric, batch.label)
+	mod.backward()
+	mod.update()
+    for name, val in metric.get_name_value():
+        logging.info('Epoch[%d] Train-%s=%f', epoch, name, val)
+    toc = time.time()
+    logging.info('Epoch[%d] Time cost=%.3f', epoch, toc-tic)
+
+    metric.reset()
+    dev_loss(mod, dev_Iter, metric)
+    curr_acc = None
+    for name, val in metric.get_name_value():
+        logging.info("Epoch[%d] Dev-%s=%f", epoch, name, val)
+	curr_acc = val
+    if epoch > 0 and curr_acc > last_acc:
+        logging.info('Epoch[%d]: Dev set performance drops, reverting this epoch', epoch)
+	logging.info('Epoch[%d]: LR decay: %g => %g', epoch, lr, lr/2.)
+	lr = lr/2.
+	mod.init_optimizer(optimizer='sgd', optimizer_params=(('learning_rate', lr), ), force_init=True)	
+	mod.set_params(*last_params)
+    else:
+    	last_params = mod.get_params()
+	last_acc = curr_acc
+	epoch += 1
+	mx.model.save_checkpoint(model_prefix, epoch, mod.symbol, *last_params)
+    if epoch == num_epoch :
+    	break
+		
+	
+
+
+
+'''
+metric = mx.metric.create('rmse')
+print "begin training"
+mod.fit(train_data=train_Iter, 
+              eval_data=dev_Iter,
+              optimizer='sgd', 
+              optimizer_params={'learning_rate':lr}, 
+              eval_metric=metric, 
+              num_epoch=num_epoch,
+              epoch_end_callback=checkpoint)  
+
+
+
+##### predict in dev set  #####
+print 'predict in dev set'
+fin = codecs.open(dev_feats_scp, 'r')
+files = fin.readlines()
+fin.close()
+
+fout = codecs.open("./result/dnn/%s.dev.result"% pfix, "w")
+for F in files:
+    basename = F.strip().split('/')[-1].split('.')[0]
+    test_Iter = Test_Iter(F, data_names, data_shapes, label_names, label_shapes, batch_size)
+    evaluation = 0
+    num_batch = 0
+    for preds, i_batch, batch in  mod.iter_predict(test_Iter):
+        num_batch += 1
+	prediction = mod.get_outputs()
+        evaluation += prediction[0].asnumpy()[:, 0].astype('float32').sum(0)/batch_size
+    evaluation = evaluation / num_batch
+    fout.write(basename + ' ' + str(evaluation) + '\n')
+fout.close()
+
+
+##### predict in eval set #####
+print 'predict in eval set'
+fin = codecs.open(eval_feats_scp, 'r')
+files = fin.readlines()
+fin.close()
+
+fout = codecs.open("./result/dnn/%s.eval.result"% pfix, "w")
+for F in files:
+    basename = F.strip().split('/')[-1].split('.')[0]
+    test_Iter = Test_Iter(F, data_names, data_shapes, label_names, label_shapes, batch_size)
+    evaluation = 0 
+    num_batch = 0
+    for preds, i_batch, batch in  mod.iter_predict(test_Iter):
+        num_batch += 1
+        prediction = mod.get_outputs()
+        evaluation += prediction[0].asnumpy()[:, 0].astype('float32').sum(0)/batch_size 
+    evaluation = evaluation / num_batch
+    fout.write(basename + ' ' + str(evaluation) + '\n')
+fout.close()
+
+'''
+conf = []
+prediction = []
+for sent, sent_pad in sent_Iter:
+    sent_pred = []
+    #print "pad:", sent_pad
+    label = sent[0].label
+    for idx in range(len(sent)):
+        batch = sent[idx]
+        mod.forward(batch, is_train=False)
+        outputs = [out[0:out.shape[0]-sent_pad] for out in mod.get_outputs()]
+
+        #print outputs;raw_input('outputs')
+
+        sent_pred.append(outputs[0])
+
+    true  = []
+    spoof = []
+    for i in range(len(sent_pred)):
+             
+        for j in range((sent_pred[i].shape[0])):
+            true.append(sent_pred[i][j][0])
+            spoof.append(sent_pred[i][j][1])
+
+    temp = 0.
+    for i in range(len(true)):
+        temp += true[i]
+    #print temp,len(true);raw_input('true')
+    true_conf = temp/len(true)
+    
+    temp = 0.
+    for i in range(len(spoof)):  
+        temp += spoof[i]
+    spoof_conf = temp/len(spoof)
+    #print temp, len(spoof);raw_input('spoof')
+    true_conf = true_conf.asnumpy()
+    prediction.append(true_conf)
+    spoof_conf = spoof_conf.asnumpy()
+
+    true  = label[0][0][0]
+    spoof = label[0][0][1]
+    true = true.asnumpy()
+    spoof = spoof.asnumpy()
+    conf.append(true_conf)
+    #print true_conf, true;
+    #if true_conf > spoof_conf:
+    #    prediction.append((1, true))
+    #else :
+    #    prediction.append((0, true))
+
+resultfn = "result/spoofing_mfcc_arc-%dx%d_lr-%f_batchsize-%d.result" % (ndnn, dnnsize, lr, batch_size) 
+fout = codecs.open(resultfn, 'w')
+dev_scp = "dev.scp"
+fin = codecs.open(dev_scp, 'r');fns = fin.readlines();fin.close()
+assert(len(fns)==len(prediction))
+for i in range(len(fns)):
+    fout.write(fns[i].split('.')[0] + ' ' + str(prediction[i]) + '\n')
+'''
+'''
+correct = 0.
+for s in range(len(prediction)):
+    fout.write(str(prediction[s][0]) + ' ' + str(prediction[s][1]) + '\n')
+    if prediction[s][0]==prediction[s][1]:
+        correct +=1
+
+print correct, len(prediction)
+accuracy = correct/len(prediction)
+print accuracy
+
+true_conf_mean = np.sum(conf[0:len(conf)/2])/(len(conf)/2)
+spoof_conf_mean = np.sum(conf[len(conf)/2:])/(len(conf)/2)
+
+print "mean:", true_conf_mean, spoof_conf_mean
+
+fout.write("\nresult:\n" + "correct: " + str(correct) + "\nall: " + str(len(prediction)) + "\naccuracy: " + str(accuracy))
+fout.write("\nmean:\n" + "true:" + str(true_conf_mean) + '\nspoof:' + str(spoof_conf_mean))
+fout.close()
+'''
+
